454c0197668eda41a8ac45ce2e8035b812e554ab
JinQing Lee <hourout@163.com>
2021-02-09 16:21:05 +0800

    Update kernel.py

--- milvus_kernel/kernel.py
+++ milvus_kernel/kernel.py
@@ -62,6 +62,12 @@ class MilvusKernel(Kernel):
                                 'Show all partitions in a collection',
                                 'Create an index',
                                 'Removes an index',
+                                'Flush data',
+                                'Compact all segments in a collection',
+                                'Search vectors',
+                                'Delete vectors by ID',
+                                'Insert a vector',
+                                'Select vector'
                             ],
                              'milvus sql':[
                                  'create collection test01 where dimension=128 and index_file_size=1024 and metric_type=L2',
@@ -72,6 +78,12 @@ class MilvusKernel(Kernel):
                                  'list partitions test01',
                                  'create index test01 where index_type=FLAT and nlist=4096',
                                  'drop index test01',
+                                 'flush test01, test02',
+                                 'compact test01',
+                                 "select 2, 3, 5 from test01 where top_k=1 and partition_tags='tag01' and nprobe=16",
+                                 'delete test01 by id=1',
+                                 "insert 2,3,5 from test01 where partition_tag='tag01' by id=0",
+                                 'select test01 by id=1,2,3',
                              ]}).to_html()
                     elif l.startswith('list collections'):
                         if not self.engine:
@@ -150,6 +162,47 @@ class MilvusKernel(Kernel):
                     elif l.startswith('flush '):
                         collection_list = [i.strip() for i in v[7:].strip().split(',') if len(i.strip())>0]
                         output = self.engine.compact(collection_name_array=collection_list).message
+                    elif l.startswith('select ') and ' where ' in l:
+                        partition_tags = None
+                        params = None
+                        for i in v.split(' where ')[1].split(' and '):
+                            param_list = i.split('=')
+                            if param_list[0].strip()=='top_k':
+                                top_k = int(param_list[1].strip())
+                            elif param_list[0].strip()=='partition_tags':
+                                partition_tags = [param_list[1].strip().replace("'", '')]
+                            elif param_list[0].strip()=='nprobe':
+                                param = {'nprobe': int(param_list[1].strip())}
+                        query_records = [float(i.strip()) for i in v.split(' from ')[0][6:].split(',') if len(i.strip())>0]
+                        output = self.engine.search(collection_name=v.split(' from ')[1].split(' where ')[0],
+                                                    top_k=top_k,
+                                                    query_records=query_records,
+                                                    partition_tags=partition_tags,
+                                                    params=params)
+                    elif l.startswith('delete '):
+                        output = self.engine.delete_entity_by_id(collection_name=v.split(' by ')[0][6:].strip(),
+                                                                 id_array=[int(i.strip()) for i in v.split('=')[1].split(',') if len(i.strip())>0]).message
+                    elif l.startswith('insert '):
+                        collection_name = v.split(' from ')[1].split(' ')[0] if ' where ' in l or ' by ' in l else v.split(' from ')[1].strip()
+                        ids = None
+                        partition_tag = None
+                        if ' where ' in l:
+                            partition_tag = v.split(' where ')[1]
+                            if ' by ' in partition_tag:
+                                partition_tag = partition_tag.split(' by ')[0]
+                            partition_tag = partition_tag.split('=')[1].strip().replace("'", '')
+                        if ' by ' in l:
+                            ids = v.split(' by ')[1]
+                            if ' where ' in ids:
+                                ids = ids.split(' where ')[0]
+                            ids = int(ids.split('=')[1].strip())
+                        records = [float(i.strip()) for i in v.split(' from ')[0][6:].split(',') if len(i.strip())>0]
+                        _, output = self.engine.insert(collection_name=collection_name, records=records, ids=ids, partition_tag=partition_tag)
+                        output = str(output)
+                    elif l.startswith('select ') and ' by ' in l:
+                        _, output = self.engine.get_entity_by_id(collection_name=v.split(' by ')[0][6:].strip(),
+                                                              ids=[int(i.strip()) for i in v.split('=')[1].split(',') if len(i.strip())>0])
+                        output = str(output)
             self.output(output)
             return self.ok()
         except Exception as msg:
